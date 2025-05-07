b70d53de51e035c080c8262d91c6df9cfc13d5f9
shadowfer <fernely24k@gmail.com>
2025-05-07 13:00:33 -0600

    First Commit

---
+++ src/test/java/com/tecdesoftware/market/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
